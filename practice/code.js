a384cf954c38f7e5abcdc2c83b54865db52d8b14
MOSTAIN BILLAH <bill.rassel@gmail.com>
2022-08-04 21:18:51 +0600

    add code.js file

---
+++ practice/code.js
@@ -0,0 +1,22 @@
+
+// console.log("Hello and welcome to the JavaScript world of practice!");
+// let grade, finalGrade, remainder, difference;
+// grade=73;
+// remainder=grade%5;
+// difference=5-remainder;
+// finalGrade=grade;
+
+// if (difference<3 && grade>39){
+//     finalGrade=grade+difference;
+// }
+// console.log(`${remainder}-${difference}-${finalGrade}`);
+
+let grades = [10, 73, 50, 67];
+
+for (const grd of grades) {
+    console.log(grd);
+}
+
+// grades.forEach(element => {
+//     console.log(element);
+// });
